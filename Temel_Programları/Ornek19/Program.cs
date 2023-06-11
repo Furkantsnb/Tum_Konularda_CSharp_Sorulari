187647fe1a57ac64adfae2f45bde01a80fe8b2f0
Furkan Taşan <95514574+Furkantsnb@users.noreply.github.com>
2023-06-11 18:38:44 +0300

    Temel Programlar

---
+++ Temel_Programları/Ornek19/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Ornek19
+//Çarpım tablosunu yazdırmak için C# Programı
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi;
+            Console.WriteLine("Bir sayi girin");
+            sayi = Convert.ToInt32(Console.ReadLine());
+
+            while (sayi <= 0)
+            {
+                Console.WriteLine("Geçersiz bir sayi girdiniz");
+                Console.Write("Sıfırdan büyük bir sayı girin");
+                sayi= Convert.ToInt32(Console.ReadLine());  
+            }
+            Console.WriteLine("Çarpım tablosu : ");
+            for(int i=1; i<sayi; i++)
+            {
+                Console.WriteLine("\n");
+                for (int j =1; j<sayi; j++)
+                {
+                    Console.WriteLine("{0}*{1} = {2}",i,j,i*j);
+                    
+
+                }
+               
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Temel_Programları/Ornek20/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Ornek20
+//Bir Notu Okumak ve Eşdeğer Açıklamayı Görüntülemek için C# Programı
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Notunuzu Girin : ");
+            char not = Convert.ToChar(Console.ReadLine());
+
+            switch (not)
+            {
+                case 'A':
+                    Console.Out.WriteLine("Süper");
+                    break;
+                case 'B':
+                    Console.Out.WriteLine("iyi");
+                    break;
+
+                case 'C':
+                    Console.Out.WriteLine("orta");
+                    break;
+                case 'D':
+                    Console.Out.WriteLine("kotu");
+                    break;
+                default:
+                    Console.Out.WriteLine("Geçersiz işlem");
+                    break;
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
