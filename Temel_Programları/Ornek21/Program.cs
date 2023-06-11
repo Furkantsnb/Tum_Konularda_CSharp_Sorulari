ddd42a99217bf2e2bdf14532f1ee5a1be8fbdbc7
Furkan Taşan <95514574+Furkantsnb@users.noreply.github.com>
2023-06-11 18:39:24 +0300

    Temel Programlar

---
+++ Temel_Programları/Ornek21/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Ornek21
+    //Küçük Harf Karakterlerini Büyük Harfe ve Tam Tersine Çevirmek için C# Programı
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char a;
+            int i;
+            Console.Write("bir karakter girin : ");
+            a = Convert.ToChar(Console.ReadLine());
+            i =(int) a;
+
+            if(a>=65 && a <= 90)
+            {
+            Console.Write("Girdiğiniz karakterin küçük harfe donuşmuş hali : {0} ", char.ToLower(a));
+                
+            }
+            else if (a>=97 && a <= 122)
+            {
+            Console.WriteLine("Girdiğiniz karakterin büyül harflere donuşmuş hali : {0} ", char.ToUpper(a));
+
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Temel_Programları/Ornek22/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Ornek22
+//Bir Kişinin Boyunu Kabul Etmek ve Daha Uzun, Cüce ve Ortalama Olarak Sınıflandırmak İçin C# Programı
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Boyunuzu Girin : ");
+            int boy = Convert.ToInt32(Console.ReadLine());
+
+            if (boy > 180)
+                Console.WriteLine("Uzun");
+            else if (boy > 165 && boy < 180)
+                Console.WriteLine("Normal");
+
+            else
+                Console.WriteLine("Kısa");
+
+        }
+    }
+}
