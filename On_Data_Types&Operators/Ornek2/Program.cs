46c421f0938ea3d5ab4f53d67aabf938fdd4fd76
Furkan Taşan <95514574+Furkantsnb@users.noreply.github.com>
2023-06-12 11:26:18 +0300

    On Data Types & Operators

---
+++ On_Data_Types&Operators/Ornek2/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Ornek2
+//Maksimum Veri Türü Aralığını Bulan C# Programı
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Maximum Range of the Decimal " +
+                              "Data Type is : {0} ", Decimal.MaxValue);
+            Console.WriteLine("The Maximum Range of the Float " +
+                              "Data Type is : {0} ", Single.MaxValue);
+            Console.WriteLine("The Maximum Range of the Decimal " +
+                              "Data Type is : {0} ", Double.MaxValue);
+            Console.WriteLine("Exponent Form : The Maximum Range of Decimal " +
+                              "Data Type  is : {0:E}", Decimal.MaxValue);
+            Console.WriteLine("Exponent Form : The Maximum Range of Float " +
+                              "Data Type  is : {0:E}", Single.MaxValue);
+            Console.WriteLine("Exponent Form : The Maximum Range of Double " +
+                              "Data Type  is : {0:E}", Double.MaxValue);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ On_Data_Types&Operators/Ornek4/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Ornek4
+//Koşullu Mantıksal Operatörün kullanımını Gösteren C# Programı
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("yaşınızı girin : ");
+            int yas = int.Parse(Console.ReadLine());
+            bool yetiskin = yas >= 18? true: false;
+            Console.WriteLine("yetişkin : {0}",yetiskin);
+            Console.ReadLine();
+        }
+    }
+}
