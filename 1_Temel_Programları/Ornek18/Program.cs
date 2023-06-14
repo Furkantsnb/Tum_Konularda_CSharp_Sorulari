86b89620e39d6f9fa5ec5c95375c520f4cf5833a
Furkan Taşan <95514574+Furkantsnb@users.noreply.github.com>
2023-06-14 22:22:31 +0300

    C#

---
+++ 1_Temel_Programları/Ornek18/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Transactions;
+
+namespace Ornek18
+//Özyineleme Kullanarak Bir Tamsayının İkili Eşdeğerini Yazdırmak İçin C# Programı
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            prog pr = new prog();
+            Console.WriteLine("Enter a decimal number : ");
+            num = int.Parse(Console.ReadLine());
+            Console.WriteLine("The binary equvalent of num is : ");
+            pr.binaryConversion(num);
+            Console.ReadLine();
+        }
+    }
+    public class prog
+    {
+        public int binaryConversion(int num)
+        {
+            int bin;
+            if(num != 0)
+            {
+                bin = (num%2) + 10 * binaryConversion(num/2);
+                Console.Write(bin);
+                return 0;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+
+}
---
+++ 3_ProgramsOnNumbers/Ornek1/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Ornek1
+//C# ile Fibonacci Dizisi Programı
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, sınır, f1 = 0, f2 = 1, f3 = 0; ;
+            Console.Write("sınırı girin : "); sınır=int.Parse(Console.ReadLine());
+            Console.Write(f1+" ");
+            Console.Write(f2+" ");
+
+            for (i = 0; i <= sınır; i++)
+            {
+                f3 = f1 + f2;
+                Console.Write("  "+f3);
+                f1 = f2;
+                f2 = f3;
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
