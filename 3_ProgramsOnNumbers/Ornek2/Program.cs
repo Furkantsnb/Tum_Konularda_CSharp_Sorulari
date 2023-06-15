83cbd38a1ca75bb148c420316bb1abcbbc80e567
Furkan Taşan <95514574+Furkantsnb@users.noreply.github.com>
2023-06-15 20:07:16 +0300

    C#

---
+++ 3_ProgramsOnNumbers/Ornek2/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Ornek2
+//Belirli Bir Sayının Faktöriyelini Yazdırmak İçin C# Programı
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi, faktoriyel = 1;
+            Console.Write("sayi girin : ");
+            sayi = int.Parse(Console.ReadLine());
+
+            for(int i = 1; i <=sayi; i++)
+            {
+                faktoriyel *=i;
+            }
+            Console.Write("Girilen sayının faktoriyeli : {0}",faktoriyel);
+        }
+    }
+}
---
+++ 3_ProgramsOnNumbers/Ornek3/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Ornek3
+//1'den 100'e Kadar Tüm Asal Sayıları Yazdıran C# Programı
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool asal = true;
+            Console.WriteLine("1'den 100'e Kadar Tüm Asal Sayılar");
+
+            for(int i = 2;i < 100;i++)
+            {
+                for(int j = 2;j < 100; j++)
+                {
+                    if(i != j && i % j == 0)
+                    {
+                        asal = false;
+                        break;
+                    }
+                }
+                if(asal)
+                {
+                    Console.Write("\t"+i);
+                }
+                asal = true;
+            }
+            Console.ReadLine();
+        }
+    }
+}
